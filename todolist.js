08453f10a8b993cedcf33e1113158cfa49363775
Christiana <christianalukinga010@gmail.com>
2024-11-26 08:37:09 +0000

    first commit

---
+++ todolist.js
@@ -0,0 +1,42 @@
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const addButton = document.getElementById('add-btn');
+
+addButton.addEventListener('click', function () {
+    var task = prompt('Ajoutez la tâche :');
+    if (task) {
+        addTask(task);
+    }
+});
+
+function addTask(task) {
+    const li = document.createElement('li');
+
+    li.innerHTML = `
+        <input type="checkbox" class="task-checkbox">
+        <span>${task}</span>
+        <span class="delete">Delete</span>
+    `;
+
+    li.querySelector('.task-checkbox').addEventListener('change', function () {
+        li.classList.toggle('completed'); 
+    });
+    
+    li.querySelector('.delete').addEventListener('click', function () {
+        li.remove(); 
+    });
+
+    taskList.appendChild(li);
+}
+
+taskInput.addEventListener('input', function() {
+    var recherche = taskInput.value.trim();
+    var tasks = document.querySelectorAll('li');
+    tasks.forEach(function(task) {
+        if (task.textContent.toLowerCase().includes(recherche.toLowerCase())) {
+            task.style.display = 'flex';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+});
